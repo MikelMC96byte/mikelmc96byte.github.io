eda3a3d60304678904faa2f7c93bc8c27bc41bbd
Mikel Maldonado <71812437+MikelMC96byte@users.noreply.github.com>
2024-06-23 14:23:19 +0200

    publish under custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://mikelmc96byte.github.io',
+    site: 'https://mikelmc.dev',
 });
